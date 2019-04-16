8b139af752b64e9aac95a418d16ade0f6e2b2c9a
Zishan Ali <zishanali06@gmail.com>
2019-04-16 17:56:30 -0500

    added donations page

---
+++ src/client/components/Donations.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+
+
+export default class Donations extends React.Component<DonationsProps, DonationsState> {
+    constructor(props: DonationsProps) {
+        super(props);
+        this.state = {
+            name: "",
+            amount: ""
+        };
+    }
+    render() {
+        return (
+            <>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            <h1>Testing Page</h1>
+            </>
+        );
+    }
+}
+
+export interface DonationsProps {
+
+}
+
+export interface DonationsState {
+
+}
